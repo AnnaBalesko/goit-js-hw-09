dac7ad98d19b318dadbd8340a743c477bf0b999b
Anna Balesko <babasheldona@icloud.com>
2025-10-03 00:00:05 -1100

    add form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,45 @@
+let formData = {
+  email: '',
+  message: '',
+};
+const STORAGE_KEY = 'feedback-form-state';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', onText);
+form.addEventListener('submit', handlerSubmit);
+
+
+function onText(event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateText() {
+  const message = localStorage.getItem(STORAGE_KEY);
+  if (message) {
+    const parsedData = JSON.parse(message);
+    document.querySelector('input[name="email"]').value =
+      parsedData.email || '';
+    document.querySelector('textarea[name="message"]').value =
+      parsedData.message || '';
+  }
+}
+
+populateText();
+
+function handlerSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem(STORAGE_KEY);
+
+    event.currentTarget.reset();
+
+    formData = { email: '', message: '' };
+  }
+}
